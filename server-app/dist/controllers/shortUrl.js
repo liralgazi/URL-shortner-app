cab511ed5d3f70a525fbaac9b20f0f8c1e67ea24
liralgazi <liralgazi@gmail.com>
2025-02-10 19:39:42 +0200

    server side is ready

---
+++ server-app/dist/controllers/shortUrl.js
@@ -0,0 +1,63 @@
+import { urlModel } from "../model/shortUrl.js";
+// creating the functions: 
+export const createUrl = async (req, res) => {
+    try {
+        console.log("The full URL is: ", req.body.fullUrl);
+        const { fullUrl } = req.body;
+        const urlFound = await urlModel.find({ fullUrl });
+        //if url found
+        if (urlFound.length > 0) {
+            res.status(409);
+            res.send(urlFound);
+        }
+        else {
+            const shortUrl = await urlModel.create({ fullUrl });
+            res.status(201).send(shortUrl);
+        }
+    }
+    catch (error) {
+        res.status(500).send("something went wrong");
+    }
+};
+export const getAllUrl = async (req, res) => {
+    try {
+        const shortUrls = await urlModel.find();
+        if (shortUrls.length < 0) {
+            res.status(404).send("Short URLs not found :(");
+        }
+        else {
+            res.status(200).send(shortUrls);
+        }
+    }
+    catch (error) {
+        res.status(500).send("something went wrong");
+    }
+};
+export const getUrl = async (req, res) => {
+    try {
+        const shortUrl = await urlModel.findOne({ shortUrl: req.params.id });
+        if (!shortUrl) {
+            res.status(404).send({ message: "Full URL not found :(" });
+        }
+        else {
+            shortUrl.clicks++;
+            //save the clicks
+            shortUrl.save();
+            res.redirect(`${shortUrl.fullUrl}`);
+        }
+    }
+    catch (error) {
+        res.status(500).send("something went wrong!");
+    }
+};
+export const deleteUrl = async (req, res) => {
+    try {
+        const shortUrl = await urlModel.findByIdAndDelete({ _id: req.params.id });
+        if (shortUrl) {
+            res.status(200).send({ message: "Requested URL successfully deleted" });
+        }
+    }
+    catch (error) {
+        res.status(500).send("something went wrong!");
+    }
+};
---
+++ server-app/src/model/shortUrl.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+
+const shortUrlSchema = new mongoose.Schema(
+    {
+        fullUrl: {
+            type: String,
+            required: true,
+        },
+        shortUrl: {
+            type: String,
+            required: true,
+            default: () => nanoid(10), // Generates a 10-character short URL
+        },
+        clicks: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const urlModel = mongoose.model("ShortUrl", shortUrlSchema);
